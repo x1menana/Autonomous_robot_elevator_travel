cd6ac7f9a9f3c42ab6d19fdebd1ad0a692cad404
x1menana <124448800+x1menana@users.noreply.github.com>
2023-04-21 21:06:00 -0500

    Add files via upload

--- elevator_pkg/include/elevator_pkg/ElevatorCoordinator.h
+++ elevator_pkg/include/elevator_pkg/ElevatorCoordinator.h
@@ -13,12 +13,17 @@ public:
     void callback(const amrl_msgs::ElevatorStatus::ConstPtr &msg);
     void callElevator(int floor, bool door);
     void setControlElevator(bool control);
+    bool isElevatorOnFloor();
 
 protected:
     ros::Subscriber _elevatorSub;
     ros::Publisher _elevatorPub;
+    // these fields represent the message we last sent
     int _floor; 
     bool _door;
+    //  these fields represent the actual status of the elevator
+    int actualFloor;
+    bool isDoorOpen;
     // field that determines if we want to control elevator (currently)
     bool controlElevator;
 };
